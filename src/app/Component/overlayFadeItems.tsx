9025b818555eed33f40f4fec3702a45a37bb09a7
Duyanh <duyanh3091999@gamil.com>
2024-04-04 19:31:46 +0700

    firework

--- src/app/Component/overlayFadeItems.tsx
+++ src/app/Component/overlayFadeItems.tsx
@@ -9,7 +9,8 @@ import Image from "next/image";
 import styles from "./styles.module.css";
 // import GenresMovies from "../GenresMovies/genresMovies";
 import Link from "next/link";
-// import image from "../../Asset/huong.png";
+import { useState } from "react";
+import Heart from "react-animated-heart";
 
 // import { originalPathPoster } from "@/core/constants";
 interface Props {
@@ -24,6 +25,7 @@ interface Props {
   type?: string;
   title: string;
   voteAverage?: number;
+  activeFirework: () => any;
 }
 
 const OverlayFadeRenderItem = ({
@@ -37,27 +39,28 @@ const OverlayFadeRenderItem = ({
   time,
   index,
   description,
+  activeFirework,
   type,
 }: Props) => {
+  const [isClick, setClick] = useState(false);
+
   return (
-    <div className={`${styles.container}  hover:scale-105`}>
-      <img
-        src={"/" + images}
-        alt="Example"
-        className="h-[350px] object-cover"
-      />
-      <div className={styles.overlayRed}>
+    <div className={`${styles.container} hover:scale-105 `}>
+      <div className="  ">
+        <img src={"/" + images} alt="Example" className="" />
+      </div>
+      <div
+        className={styles.overlayRed}
+        onClick={() => {
+          setClick(true);
+          activeFirework();
+        }}
+      >
         <div className={`${styles.text} line-clamp-6 grid grid-cols-1`}>
-          <div className="text-xs line-clamp-2">{title}</div>
           <div className="flex justify-center">
-            {voteAverage?.toFixed(1)}
-            {type !== "suggestAdmin" && (
-              <img alt="" src={""} className="w-[20%] ml-1" />
-            )}
+            <Heart isClick={isClick} onClick={() => setClick(!isClick)} />
           </div>
-          <Link prefetch={true} href={`movies/${id}`}>
-            {/* <Button className="text-white mt-3">Detail</Button> */}
-          </Link>
+          <div className="text-xs line-clamp-2 ">{title}</div>
           {/* <div className="text-xs">{formatDate}</div> */}
         </div>
         <div className="absolute top-[55%] left-[17%] w-[220px]">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
+"use client";
+import { useCallback, useEffect, useState } from "react";
 import OverlayFadeRenderItem from "./Component/overlayFadeItems";
-
-interface PosterMovies {
-  vote_average: number;
-  original_title: string;
-  overview: string;
-  poster_path: string;
-  genre_ids: number[];
-  id: number;
-}
+import { Fireworks } from "fireworks-js";
 
 const data = [
   {
@@ -45,6 +39,81 @@ const data = [
 ];
 
 export default function Home() {
+  const [isFireworkActive, setIsFireworkActive] = useState(false);
+
+  useEffect(() => {
+    if (isFireworkActive) {
+      const container = document?.querySelector(".container");
+      if (!container) return; // Make sure container is available
+      const fireworks = new Fireworks(container, {
+        autoresize: true,
+        opacity: 0.5,
+        acceleration: 1.05,
+        friction: 0.97,
+        gravity: 1.5,
+        particles: 309,
+        traceLength: 30,
+        traceSpeed: 10,
+        explosion: 15,
+        intensity: 30,
+        flickering: 50,
+        lineStyle: "square",
+        hue: {
+          min: 0,
+          max: 360,
+        },
+        delay: {
+          min: 30,
+          max: 60,
+        },
+        rocketsPoint: {
+          min: 0,
+          max: 100,
+        },
+        lineWidth: {
+          explosion: {
+            min: 1,
+            max: 3,
+          },
+          trace: {
+            min: 1,
+            max: 2,
+          },
+        },
+        brightness: {
+          min: 50,
+          max: 80,
+        },
+        decay: {
+          min: 0.015,
+          max: 0.03,
+        },
+        mouse: {
+          click: false,
+          move: false,
+          max: 1,
+        },
+      });
+      fireworks.start();
+
+      // Stop the fireworks after 5 seconds
+      const stopFireworks = setTimeout(() => {
+        fireworks.stop();
+        setIsFireworkActive(false);
+      }, 5000);
+
+      // Clear the timeout to stop the fireworks if component unmounts or isFireworkActive changes
+      return () => clearTimeout(stopFireworks);
+    }
+  }, [isFireworkActive]);
+
+  const handleFireworkActivation = () => {
+    // console.log("dsada");
+    setIsFireworkActive(true);
+  };
+
+  console.log(isFireworkActive);
+
   const renderPosterMovies = (item: any) => {
     return (
       <OverlayFadeRenderItem
@@ -54,6 +123,7 @@ export default function Home() {
         images={item?.image}
         genreIds={item?.genre_ids}
         voteAverage={item?.vote_average}
+        activeFirework={handleFireworkActivation}
       />
     );
   };
@@ -62,25 +132,28 @@ export default function Home() {
       <div className="">
         <div className="grid place-content-center gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
           <div className=" text-xs text-white flex justify-center">
-            {" "}
-            Ngày 29/3/2024{" "}
+            Ngày 29/3/2024
           </div>
           <div className="items-center flex justify-center">
             {" "}
             <div className="rounded-full bg-red-700 w-[10px]  h-[10px]"> </div>
           </div>
-
           {data?.map(renderPosterMovies)}
         </div>
       </div>
     );
   };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="h-[500px]">
-        {/* <CarouselMovie dataPopularMovies={popularMovies} /> */}
-        <div className="md:mx-24">{suggestUserChoiceList()}</div>
-      </div>{" "}
+    <main className="flex min-h-screen flex-col items-center justify-between p-20 relative">
+      {/* Add class "container" here */}
+      {/* Ensure the container has position:relative to establish stacking context */}
+      {isFireworkActive && (
+        <div className="firework container z-[50] absolute top-0 bottom-0 left-1/2 transform -translate-x-1/2" />
+      )}
+      {/* Place your fireworks here */}
+      {/* <CarouselMovie dataPopularMovies={popularMovies} /> */}
+      <div className="relative flex-1">{suggestUserChoiceList()}</div>
+      {/* Make sure to add z-index to the suggest to ensure it is above other content */}
     </main>
   );
 }
